f898b748034605f06bb9e973a6d93942dfb429c1
sa-bt <bakhshian2020@gmail.com>
2022-06-25 22:03:05 +0430

    oop -> phpdoc

---
+++ OOP/PhpDoc/Classes/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PhpDocs;
+
+/**
+ * This is class test PhpDoc
+ *
+ */
+
+class User
+{
+    /**
+     * @version 0.0.1
+     * @license SabtLicense
+     * @link https://sa-bt.ir
+     * @copyright 2019
+     * @author Seyed Ahmad Bakhshian
+     * @return string
+     */
+    public function returnUserName(): string
+    {
+        return "SABT";
+    }
+}
