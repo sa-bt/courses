eed0eaf5925996b2402162da971096ae5e850ff6
Pourya <bakhshian2020@gmail.com>
2022-09-16 23:13:07 +0430

    start linux course

---
+++ Linux/Script/commandSubstitution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Command Substitution Sample1 $(uname -r)"
+echo "Command Substitution Sample2 $(uname )"
+echo "Command Substitution Sample3 `who`"
+echo "Command Substitution Sample4 `whoami`"
