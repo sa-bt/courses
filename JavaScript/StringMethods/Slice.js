ac9b56f78d6f5ab90006322acf1fab709c4be4c3
Pourya <bakhshian2020@gmail.com>
2023-05-07 08:00:18 -0400

    start javascrpt learning

---
+++ JavaScript/StringMethods/Slice.js
@@ -0,0 +1,11 @@
+let str="this is a sample";
+console.log("Original String: "+ str);
+
+let fromTo=str.slice(0,5)
+console.log("Slice 0 to 5 Strings: "+ fromTo);
+
+let fromToEnd =str.slice(5)
+console.log("Slice from 5 to end Strings: "+ fromToEnd);
+
+let fromToReverse =str.slice(-9 , -1)
+console.log("Slice from -9 to -1 Strings: "+ fromToReverse);
