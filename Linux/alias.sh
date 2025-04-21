c374db15b66fc88f6433d227a99863fb0c78dc93
Pourya <bakhshian2020@gmail.com>
2025-04-21 13:56:54 +0330

    alias command script

--- Linux/alias.sh
+++ Linux/alias.sh
@@ -2,7 +2,16 @@
 read -n 3 -p "Please enter your alias name: " INP
 echo $INP
 echo
-read -t 60 -p "Please enter your command: " COM
+if [ -z "$INP" ]; then
+    echo "Alias name can not be empty."
+    exit 1
+fi
+
+read -t 60 -p "Please enter your command for $INP: " COM
 echo $COM
 echo
-`echi $INP`
+if [ -z "$COM" ]; then
+    echo "Command can not be empty."
+    exit 1
+fi
+echo "alias $INP=\"$COM\"" | bash
